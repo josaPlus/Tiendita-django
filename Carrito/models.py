11279d7eb86a0e3c9f8358c85d53e9b83bbae51b
josaPlus <josafat.saldana@hotmail.com>
2025-12-03 10:18:06 -0600

    modificacion en modelo carrito

--- Carrito/models.py
+++ Carrito/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Carrito(models.Model):
     # idCarrito es el PK automático
     # El campo JSON según tu ER. Puede usarse para guardar un snapshot o logs.
-    item = models.JSONField(default=dict, blank=True, null=True) 
     fecha = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     
